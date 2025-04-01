2f29a9b3e5f7e0fff84de4909df048f6e09ee63f
Vova Stepanyuk <stepanyukvova200@gmail.com>
2025-04-01 23:28:41 +0300

    add inter font

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,135 +1,135 @@
+import { CloseIcon, QuickAccountIcon, QuickIcon } from '../icons'
 import './style.scss'
-import {CloseIcon, QuickAccountIcon, QuickIcon} from "../icons";
 
 const Aside = () => {
   return (
     <div className="aside">
       <div className="aside__background"></div>
       <div className="aside__content">
-        <button className="aside__close-btn">
+        <button className='aside__close-btn'>
           <CloseIcon />
         </button>
         <div className="aside__content-header">Connect</div>
         <button className="aside__content-quick-account">
-          <span className="aside__content-quick-account-new">new!</span>
+          <span className="aside__content-quick-account-new">NEW!</span>
           <div className="aside__content-quick-account-info">
-            <div className="aside__content-quick-account-info-title">
-              <QuickIcon />
-              Jupiter Quick Account
+            <div>
+              <div className="aside__content-quick-account-info-title">
+                <QuickIcon />
+                Jupiter Quick Account
+              </div>
+              <div className="aside__content-quick-account-info-subtitle">
+                Trade instantly without signing transactions!
+              </div>
             </div>
-            <div className="aside__content-quick-account-info-subtitle">
-              Trade instantly without signing transactions!
-            </div>
+
             <div className="aside__content-quick-account-info-img">
               <QuickAccountIcon />
             </div>
           </div>
         </button>
-
-        <div style={{ padding: '0 4px' }}>
-          <div className="aside__content-top-wallets">
-            <div className="aside__content-top-wallets-title">Top wallets</div>
-            <button className="aside__content-top-wallets-jupiter">
+        <div className="aside__content-top-wallets">
+          <div className="aside__content-top-wallets-title">Top wallets</div>
+          <button className="aside__content-top-wallets-jupiter">
+            <img
+              src={`${process.env.PUBLIC_URL}/walletImg/jupiter.webp`}
+              alt=""
+            />
+            <div>
+              <div className="aside__content-top-wallets-jupiter-title">
+                Jupiter Mobile
+              </div>
+              <div className="aside__content-top-wallets-jupiter-subtitle">
+                On-the-go and Desktop
+              </div>
+            </div>
+          </button>
+          <div className="aside__content-top-wallets-buttons">
+            <button className="aside__content-top-wallets-button">
               <img
-                src={`${process.env.PUBLIC_URL}/walletImg/jupiter.webp`}
+                src={`${process.env.PUBLIC_URL}/walletImg/phantom.svg`}
                 alt=""
               />
-              <div>
-                <div className="aside__content-top-wallets-jupiter-title">
-                  Jupiter Mobile
-                </div>
-                <div className="aside__content-top-wallets-jupiter-subtitle">
-                  On-the-go and Desktop
-                </div>
-              </div>
             </button>
-            <div className="aside__content-top-wallets-buttons">
-              <button className="aside__content-top-wallets-button">
-                <img
-                  src={`${process.env.PUBLIC_URL}/walletImg/phantom.svg`}
-                  alt=""
-                />
-              </button>
 
-              <button className="aside__content-top-wallets-button">
-                <img
-                  src={`${process.env.PUBLIC_URL}/walletImg/solfare.svg`}
-                  alt=""
-                />
-              </button>
-              <button className="aside__content-top-wallets-button">
-                <img
-                  src={`${process.env.PUBLIC_URL}/walletImg/backpack.png`}
-                  alt=""
-                />
-              </button>
-              <button className="aside__content-top-wallets-button">
-                <img
-                  src={`${process.env.PUBLIC_URL}/walletImg/coinbase.svg`}
-                  alt=""
-                />
-              </button>
-              <button className="aside__content-top-wallets-button">
-                <img
-                  src={`${process.env.PUBLIC_URL}/walletImg/magicEden.svg`}
-                  alt=""
-                />
-              </button>
-              <button className="aside__content-top-wallets-button">
-                <img
-                  src={`${process.env.PUBLIC_URL}/walletImg/reown.svg`}
-                  alt=""
-                />
-                QR
-              </button>
-            </div>
+            <button className="aside__content-top-wallets-button">
+              <img
+                src={`${process.env.PUBLIC_URL}/walletImg/solfare.svg`}
+                alt=""
+              />
+            </button>
+            <button className="aside__content-top-wallets-button">
+              <img
+                src={`${process.env.PUBLIC_URL}/walletImg/backpack.png`}
+                alt=""
+              />
+            </button>
+            <button className="aside__content-top-wallets-button">
+              <img
+                src={`${process.env.PUBLIC_URL}/walletImg/coinbase.svg`}
+                alt=""
+              />
+            </button>
+            <button className="aside__content-top-wallets-button">
+              <img
+                src={`${process.env.PUBLIC_URL}/walletImg/magicEden.svg`}
+                alt=""
+              />
+            </button>
+            <button className="aside__content-top-wallets-button">
+              <img
+                src={`${process.env.PUBLIC_URL}/walletImg/reown.svg`}
+                alt=""
+              />
+              QR
+            </button>
           </div>
-          <div className="aside__content-more-wallets">
-            <div className="aside__content-top-wallets-title">More wallets</div>
-            <div className="aside__content-top-wallets-buttons">
-              <button className="aside__content-top-wallets-button">
-                <img
-                  src={`${process.env.PUBLIC_URL}/walletImg/trust.svg`}
-                  alt=""
-                />
-              </button>
-              <button className="aside__content-top-wallets-button">
-                <img
-                  src={`${process.env.PUBLIC_URL}/walletImg/ledger.svg`}
-                  alt=""
-                />
-              </button>
-              <button className="aside__content-top-wallets-button">
-                <img
-                  src={`${process.env.PUBLIC_URL}/walletImg/trezor.svg`}
-                  alt=""
-                />
-              </button>
-              <button className="aside__content-top-wallets-button">
-                <img
-                  src={`${process.env.PUBLIC_URL}/walletImg/ethereum.svg`}
-                  alt=""
-                />
-              </button>
-              <button className="aside__content-top-wallets-button">
-                <img
-                  src={`${process.env.PUBLIC_URL}/walletImg/coin98.svg`}
-                  alt=""
-                />
-              </button>
-              <button className="aside__content-top-wallets-button">
-                <img
-                  src={`${process.env.PUBLIC_URL}/walletImg/tipLink.svg`}
-                  alt=""
-                />
-              </button>
-              <button className="aside__content-top-wallets-button">
-                <img
-                  src={`${process.env.PUBLIC_URL}/walletImg/bitget.png`}
-                  alt=""
-                />
-              </button>
-            </div>
+        </div>
+        <div className="aside__content-more-wallets">
+          <div className="aside__content-top-wallets-title">More wallets</div>
+          <div className="aside__content-top-wallets-buttons">
+            <button className="aside__content-top-wallets-button">
+              <img
+                src={`${process.env.PUBLIC_URL}/walletImg/trust.svg`}
+                alt=""
+              />
+            </button>
+            <button className="aside__content-top-wallets-button">
+              <img
+                src={`${process.env.PUBLIC_URL}/walletImg/ledger.svg`}
+                alt=""
+              />
+            </button>
+            <button className="aside__content-top-wallets-button">
+              <img
+                src={`${process.env.PUBLIC_URL}/walletImg/trezor.svg`}
+                alt=""
+              />
+            </button>
+            <button className="aside__content-top-wallets-button">
+              <img
+                src={`${process.env.PUBLIC_URL}/walletImg/ethereum.svg`}
+                alt=""
+              />
+            </button>
+            <button className="aside__content-top-wallets-button">
+              <img
+                src={`${process.env.PUBLIC_URL}/walletImg/coin98.svg`}
+                alt=""
+              />
+            </button>
+            <button className="aside__content-top-wallets-button">
+              <img
+                src={`${process.env.PUBLIC_URL}/walletImg/tipLink.svg`}
+                alt=""
+              />
+            </button>
+            <button className="aside__content-top-wallets-button">
+              <img
+                src={`${process.env.PUBLIC_URL}/walletImg/bitget.png`}
+                alt=""
+              />
+            </button>
           </div>
         </div>
       </div>
