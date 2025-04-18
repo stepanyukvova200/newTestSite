dc88add81e2d2614111d2d7abc7985f4d6880bae
vector55764 <vectorforwork@gmail.com>
2025-04-18 20:22:53 +0300

    add mock data for more tab + icons(not all)

--- src/components/MoreItem/data.ts
+++ src/components/MoreItem/data.ts
@@ -1,17 +1,62 @@
 export const data = [
   {
+    link: 'https://jup.ag/mobile',
     icon: '',
     title: 'Mobile',
     desc: 'Trade 8-10x cheaper & 3x faster than any other wallets on a fully-native app.',
   },
   {
+    link: 'https://verify.jup.ag/',
     icon: '',
     title: 'Verify',
     desc: 'Token verification standards to boost transparency and collaboration.',
   },
   {
+    link: 'https://portfolio.jup.ag/',
     icon: '',
-    title: 'Verify',
-    desc: 'Token verification standards to boost transparency and collaboration.',
+    title: 'Portfolio',
+    desc: 'Track your portfolio, across multiple wallets, in one place.',
+  },
+  {
+    link: 'https://lock.jup.ag/',
+    icon: '',
+    title: 'Lock',
+    desc: "Open-source, audited and free to release your project's tokens over time.",
+  },
+  {
+    link: 'https://vote.jup.ag/',
+    icon: '',
+    title: 'Governance',
+    desc: 'The first voting platform for Cats in history.',
+  },
+  {
+    link: 'https://www.jupresear.ch/',
+    icon: '',
+    title: 'Research',
+    desc: 'Deep dive into discussions and research around all things Jupiter.',
+  },
+  {
+    link: 'https://portfolio.jup.ag/onramp',
+    icon: '',
+    title: 'Onboard',
+    desc: 'Buy SOL with your credit card or bridge assets from other chains.',
+  },
+  {
+    link: 'https://support.jup.ag/hc/en-us',
+    icon: '',
+    title: 'Jupiter Guides',
+    desc: 'Buy SOL with your credit card or bridge assets from other chains.',
+  },
+  {
+    link: 'https://dev.jup.ag/',
+    icon: '',
+    title: 'Jupiter Develop',
+    desc: 'Build with the most comprehensive APIs and no-code tool kits powered by Jupiter.',
+  },
+  {
+    link: 'https://terminal.jup.ag/',
+    icon: '',
+    title: 'Jupiter Terminal',
+    desc: 'Open-source, lite version of Jupiter that provides end-to-end swap with just a few lines of code.',
   },
 ]
